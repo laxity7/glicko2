e8b2df6e827dd164651c6d80fea1eff1aecad4a9
Vlad Varlamov <laxity7@gmail.com>
2023-09-28 02:15:02 +0400

    Upgrade to the latest version of PHPUnit

--- tests/Glicko2Test.php
+++ tests/Glicko2Test.php
@@ -2,24 +2,24 @@
 
 namespace laxity7\glicko2\Test;
 
-use PHPUnit_Framework_TestCase;
-use laxity7\glicko2\MatchGame;
 use laxity7\glicko2\MatchCollection;
+use laxity7\glicko2\MatchGame;
 use laxity7\glicko2\Player;
+use PHPUnit\Framework\TestCase;
 
-final class Glicko2Test extends PHPUnit_Framework_TestCase
+final class Glicko2Test extends TestCase
 {
 
-    public function testDefaultPlayer()
+    public function testDefaultPlayer(): void
     {
         $player = new Player();
 
-        $this->assertEquals(Player::DEFAULT_RATING, $player->getRating());
-        $this->assertEquals(Player::DEFAULT_RATING_DEVIATION, $player->getRatingDeviation());
-        $this->assertEquals(Player::DEFAULT_RATING_VOLATILITY, $player->getRatingVolatility());
+        self::assertEquals(Player::DEFAULT_RATING, $player->getRating());
+        self::assertEquals(Player::DEFAULT_RATING_DEVIATION, $player->getRatingDeviation());
+        self::assertEquals(Player::DEFAULT_RATING_VOLATILITY, $player->getRatingVolatility());
     }
 
-    public function testCustomPlayer()
+    public function testCustomPlayer(): void
     {
         $r = 1700;
         $rd = 300;
@@ -27,12 +27,12 @@ final class Glicko2Test extends PHPUnit_Framework_TestCase
 
         $player = new Player($r, $rd, $sigma);
 
-        $this->assertEquals($r, $player->getRating());
-        $this->assertEquals($rd, $player->getRatingDeviation());
-        $this->assertEquals($sigma, $player->getRatingVolatility());
+        self::assertEquals($r, $player->getRating());
+        self::assertEquals($rd, $player->getRatingDeviation());
+        self::assertEquals($sigma, $player->getRatingVolatility());
     }
 
-    public function testCalculateMatch()
+    public function testCalculateMatch(): void
     {
         $player1 = new Player(1500, 200, 0.06);
         $player2 = new Player(1400, 30, 0.06);
@@ -40,16 +40,16 @@ final class Glicko2Test extends PHPUnit_Framework_TestCase
         $match = new MatchGame($player1, $player2, 1, 0);
         $match->calculate();
 
-        $this->assertEquals(1563.564, $this->round($player1->getRating()));
-        $this->assertEquals(175.403, $this->round($player1->getRatingDeviation()));
-        $this->assertEquals(0.06, $this->round($player1->getRatingVolatility()));
+        self::assertEquals(1563.564, $this->round($player1->getRating()));
+        self::assertEquals(175.403, $this->round($player1->getRatingDeviation()));
+        self::assertEquals(0.06, $this->round($player1->getRatingVolatility()));
 
-        $this->assertEquals(1398.144, $this->round($player2->getRating()));
-        $this->assertEquals(31.67, $this->round($player2->getRatingDeviation()));
-        $this->assertEquals(0.06, $this->round($player2->getRatingVolatility()));
+        self::assertEquals(1398.144, $this->round($player2->getRating()));
+        self::assertEquals(31.67, $this->round($player2->getRatingDeviation()));
+        self::assertEquals(0.06, $this->round($player2->getRatingVolatility()));
     }
 
-    public function testCalculateMatchCollection()
+    public function testCalculateMatchCollection(): void
     {
         $player1 = new Player(1500, 200, 0.06);
         $player2 = new Player(1400, 30, 0.06);
@@ -67,22 +67,24 @@ final class Glicko2Test extends PHPUnit_Framework_TestCase
         $matchCollection->addMatch(new MatchGame($player3, $player4, 1, 0));
         $matchCollection->calculate();
 
-        $this->assertEquals($this->round($player1->getRating()), $this->round($player3->getRating()));
-        $this->assertEquals($this->round($player2->getRating()), $this->round($player4->getRating()));
-        $this->assertEquals($this->round($player1->getRatingDeviation()), $this->round($player3->getRatingDeviation()));
-        $this->assertEquals($this->round($player2->getRatingDeviation()), $this->round($player4->getRatingDeviation()));
-        $this->assertEquals($this->round($player1->getRatingVolatility()), $this->round($player3->getRatingVolatility()));
-        $this->assertEquals($this->round($player2->getRatingVolatility()), $this->round($player4->getRatingVolatility()));
+        self::assertEquals($this->round($player1->getRating()), $this->round($player3->getRating()));
+        self::assertEquals($this->round($player2->getRating()), $this->round($player4->getRating()));
+        self::assertEquals($this->round($player1->getRatingDeviation()), $this->round($player3->getRatingDeviation()));
+        self::assertEquals($this->round($player2->getRatingDeviation()), $this->round($player4->getRatingDeviation()));
+        self::assertEquals(
+            $this->round($player1->getRatingVolatility()),
+            $this->round($player3->getRatingVolatility())
+        );
+        self::assertEquals(
+            $this->round($player2->getRatingVolatility()),
+            $this->round($player4->getRatingVolatility())
+        );
     }
 
     /**
      * For different platforms compatibility
-     *
-     * @param float $value
-     *
-     * @return float
      */
-    private function round($value)
+    private function round(float $value): float
     {
         return round($value, 3);
     }
